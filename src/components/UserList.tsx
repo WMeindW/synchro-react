f7595e051f49ef6e9f75f52c734af377b6609577
WMeindW <daniellinda2005@gmail.com>
2024-11-12 19:37:09 +0100

    update

--- src/components/UserList.tsx
+++ src/components/UserList.tsx
@@ -1,6 +1,7 @@
-import React, {ReactElement, ReactNode, useEffect, useState} from "react";
+import React, {ReactElement, useEffect, useState} from "react";
 
 interface User {
+    id: string
     username: string
     role: string
     enabled: string
@@ -11,7 +12,7 @@ interface UserList {
 }
 
 export default function UserList() {
-    const [users, setUsers] = useState<UserList>({users: []});
+    const [users, setUsers] = useState({users: [<div key={0}></div>]});
 
     async function queryUsers(): Promise<UserList[]> {
         try {
@@ -25,16 +26,23 @@ export default function UserList() {
         }
     }
 
-    function processUsers(users: User[]): ReactElement<any, any> {
-        return React.createElement("div")
+    function processUsers(users: User[]) {
+        let children: ReactElement[] = [];
+        console.log(users);
+        if (users)
+            users.forEach(user => {
+                children.push(React.createElement("div", {key: user.id}, user.username + " " + user.enabled + " " + user.id))
+            })
+        setUsers({...users, users: children});
+
     }
 
     useEffect(() => {
         console.log("querying users...");
         queryUsers().then((usersList) => {
             // @ts-ignore
-            processUsers(usersList["userList"]);
+            processUsers(usersList["userList"])
         })
     }, []);
-    return <div>{processUsers(users.users)}</div>
+    return <div><br/>{users.users}<br/></div>
 }
