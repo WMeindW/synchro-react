4e8a9c42996411e8751b5164149d73c3edaadf82
Meind <105383135+WMeindW@users.noreply.github.com>
2024-11-25 08:19:49 +0100

    update TODO: Save motd function

--- src/components/CreateMotd.tsx
+++ src/components/CreateMotd.tsx
@@ -14,10 +14,10 @@ export default function CreateMotd() {
         });
     };
     return <div>
-        <div style={{minWidth: "100vw", height: "auto", border: "2px solid black", marginBottom: "50px"}}
+        <div style={{width: "100%", minHeight: "50px", padding:"0",margin: "0", border: "1px solid black", marginBottom: "10px"}}
              dangerouslySetInnerHTML={renderedMotd}></div>
         <input onChange={handleChange} value={formData.motd} type={"textarea"}
-               style={{minWidth: "100vw", height: "auto", border: "2px solid black", marginBottom: "50px"}}
+               style={{width: "100%", minHeight: "50px",padding:"0", margin: "0", border: "1px solid black", marginBottom: "10px"}}
                placeholder={"Motd..."}></input>
     </div>
 }
