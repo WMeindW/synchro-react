6e52172452ee3078593b201e4c68e25debc1b5b9
WMeindW <daniellinda2005@gmail.com>
2024-12-17 18:12:31 +0100

    deploy

--- src/components/BarChart.tsx
+++ src/components/BarChart.tsx
@@ -1,4 +1,3 @@
-// BarChartDemo.js
 import { Bar } from 'react-chartjs-2';
 import {
     Chart as ChartJS,
--- src/config/SynchroConfig.ts
+++ src/config/SynchroConfig.ts
@@ -1,3 +1,3 @@
 export class SynchroConfig {
-    static readonly apiUrl: string = "http://localhost:8083/"
+    static readonly apiUrl: string = "https://daniellinda.net/synchro/api/"
 }
