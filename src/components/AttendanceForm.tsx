da5ffaf40be59ded03bc4e3c34a03693f9cebc35
WMeindW <daniellinda2005@gmail.com>
2025-01-21 21:00:27 +0100

    update

--- src/components/AttendanceForm.tsx
+++ src/components/AttendanceForm.tsx
@@ -2,6 +2,7 @@ import React, {useEffect, useState} from "react";
 import {SynchroConfig} from "../config/SynchroConfig.ts"
 import {Parser} from "../service/Parser.ts";
 import {Client} from "../service/Client.ts";
+import moment from "moment/moment";
 
 export default function AttendanceForm() {
     // State to store form data
@@ -44,8 +45,8 @@ export default function AttendanceForm() {
     }, []);
     // Handle form submission
     const handleSubmit = (e: React.FormEvent) => {
-        e.preventDefault(); // Prevent the default form submission
-        console.log("Cookie: " + document.cookie)
+        e.preventDefault();
+        console.log("username: " + formData.username)
         fetch(SynchroConfig.apiUrl + "user/check-attendance", {
             method: "POST",
             headers: {
@@ -66,10 +67,17 @@ export default function AttendanceForm() {
         return "Check-In"
     }
     return (
-        <form onSubmit={handleSubmit}>
-            <label>Attendance</label>
+        <form className={"attendance-form"} onSubmit={handleSubmit}>
+            <svg width="2em" height="2em" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                <path
+                    d="M12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22Z"
+                    fill="#043C5E"/>
+                <path
+                    d="M12 7.25C12.4142 7.25 12.75 7.58579 12.75 8V11.6893L15.0303 13.9697C15.3232 14.2626 15.3232 14.7374 15.0303 15.0303C14.7374 15.3232 14.2626 15.3232 13.9697 15.0303L11.4697 12.5303C11.329 12.3897 11.25 12.1989 11.25 12V8C11.25 7.58579 11.5858 7.25 12 7.25Z"
+                    fill="#DFD5A5"/>
+            </svg>
             <select dangerouslySetInnerHTML={{__html: Parser.parseUsers()}} name="username"
-                    value={formData.username} onChange={handleChange}>
+                    defaultValue={formData.username} onChange={handleChange}>
             </select>
             <button className={"button"} type="submit">{handleChecks(checkedIn.checkedIn)}</button>
         </form>
