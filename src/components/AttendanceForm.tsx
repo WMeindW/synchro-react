3410f2934c67265328995fb12ea365d40bf63a54
Meind <105383135+WMeindW@users.noreply.github.com>
2025-02-24 12:52:00 +0100

    update

--- src/components/AttendanceForm.tsx
+++ src/components/AttendanceForm.tsx
@@ -67,7 +67,7 @@ export default function AttendanceForm() {
     }
     return (
         <form className={"container-form"} onSubmit={handleSubmit}>
-            <svg width="2em" height="2em" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+            <svg className={"attendance-clock"} width="2em" height="2em" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                 <path
                     d="M12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22Z"
                     fill="#3E5879"/>
